f2b45f42ee009a6706db8f47403ef2f016896eb4
pratap562 <kumpratap562@gmail.com>
2023-03-03 19:21:36 +0530

    deleted older code

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-require('dotenv').config()
-
-module.exports = nextConfig
--- pages/about.js
+++
@@ -1,9 +0,0 @@
-import { Navbar } from "@/component/navbar"
-export default function About() {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <div>about page</div>
-        </div>
-    )
-}
